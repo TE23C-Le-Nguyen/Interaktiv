c5d82e3cae5b667aab1f7d43c4dfa4a495de3cc1
Le gia Nguyen <le.gia@elev.ga.ntig.se>
2024-09-05 12:23:41 +0200

    rjrjnrfkjnlfkjn

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,14 +1,18 @@
-﻿
-
-Console.WriteLine("Användarnamn");
+﻿Console.WriteLine("Användarnamn");
 string name = Console.ReadLine();
+name = name.ToLower();
 
-if (name == "Nguyen")
+if (name.ToLower() == "Nguyen" || name == "martin")
+{
+    Console.WriteLine("Välkommen!");
+}
+else if (name == "martin")
 {
     Console.WriteLine("Välkommen!");
 }
 else
 {
-    Console.WriteLine("FU");
+        Console.WriteLine("FU");
 }
+
 Console.ReadLine();
